5a580ae0bbaeb93845f982c8f6950d9cfe9d499c
Daniel Hocevar <danielhocevar1@gmail.com>
2021-04-02 15:19:09 -0400

    Backup

--- assemble_data.py
+++ assemble_data.py
@@ -6,27 +6,31 @@ Copyright, Daniel Hocevar and Roman Zupancic
 import requests
 import json
 
-def get_packages(package_names: list[str]) -> list[dict]:
+def get_packages(package_name: str) -> list[dict]:
     """Query npms.io for package data corresponding to package_name.
     """
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-    } 
-    r = requests.get(f'https://api.npms.io/v2/package/mget',
-                     headers=headers, data=str(package_names))
+    }
 
+    data = r'{ "keys": ["react", "firebase"] }'
+    print(data)
+    
+    r = requests.post(f'https://replicate.npmjs.com/_all_docs?include_docs=true',
+                      headers = headers,
+                      data = str(data))
+
+    print(r.text)
     # Ensure it doesn't error
     if r.status_code == 200:
-        print(r.text)
-        packages = json.loads(r.text)
+        package = json.loads(r.text)
 
-        for package in packages:
-            _trim_package_data(package[package])
+        _trim_package_data(package[package])
 
-        print(packages)
+        print(package)
 
-        return packages
+        return package
     else:
         raise KeyError('Package Name is invalid, or the webpage is unavailable')
 
@@ -50,33 +54,67 @@ def _trim_package_data(data: dict) -> None:
     data['collected'].pop('source')
 
 
-def get_sample_package_names() -> set[str]:
-    keywords = ['database',
-                'interface',
-                'state management',
-                'interaction',
-                'animations',
-                'payments',
-                'authentication',
-                'themes',
-                'deep learning',
-                '3d',
-                'machine learning',
-                'data science',
-                'storage',
-                'performance',
-                'optimization',
-                'analytics',
-                'query',
-                'cloud']
-    packages = {}
-    for keyword in keywords:
-        r = requests.get("https://api.npms.io/v2/search/suggestions?q=" + keyword)
-        results = r['results']
-        for package in results:
-            package.add(package['name'])
+def get_dependencies():
+    with open('package_list.txt', 'r') as file:
+        data = file.read()
+        i = 0
+        for line in data:
+            r = requests.get("https://api.npms.io/v2/package/" + line)
+            print(i)
+            i += 1
+        
+    # r = requests.get("https://api.npms.io/v2/package/graphql")
+
+    # results = json.loads(r.text)
+    # print(results)
+
+
+def write_sample_package_names() -> set[str]:
+    """
+    Write a list of packages to a text file
+    """
+    packages = set()
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    for char in chars:
+        print(char)
+        for i in range(10):
+            start = 250 * i
+            r = requests.get("https://api.npms.io/v2/search?from=" + str(start) + "&" + "size=250&q=" + char + "+boost-exact:false")
+            results = json.loads(r.text)
+            results = results['results']
+            for package in results:
+                packages.add(package['package']['name'])
+    print('Starting to write')
+    with open('package_list.txt', 'w') as file:
+        for package in packages:
+            file.write(package + '\n')
+    
     return packages
 
+
+def write_popular_package_names() -> set[str]:
+    """
+    Write a list of popular packages to a text file
+    """
+    packages = set()
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    chars = 'a'
+    for char in chars:
+        print(char)
+        for i in range(10):
+            start = 250 * i
+            r = requests.get("https://api.npms.io/v2/search?from=" + str(start) + "&" + "size=250&q=" + char + "+boost-exact:false")
+            results = json.loads(r.text)
+            results = results['results']
+            print(results)
+            for package in results:
+                packages.add(package['package']['name'])
+    print('Starting to write')
+    with open('package_list.txt', 'w') as file:
+        for package in packages:
+            file.write(package + '\n')
+    
+    return packages
 
 
 if __name__ == '__main__':
