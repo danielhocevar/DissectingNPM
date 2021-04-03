c193509e103c3abe63a762b3a04a97a55e7c707f
Daniel Hocevar <danielhocevar1@gmail.com>
2021-04-03 16:11:57 -0400

    Semi-Working package info downloader

--- assemble_data.py
+++ assemble_data.py
@@ -5,35 +5,41 @@ Copyright, Daniel Hocevar and Roman Zupancic
 
 import requests
 import json
+import pandas as pd
 
-def get_packages(package_name: str) -> list[dict]:
+HEADERS = [
+    'name',
+    'version',
+    'description',
+    'keywords',
+    'dependencies',
+    'devDependencies',
+    'communityInterest',
+    'downloadsCount',
+    'downloadsAcceleration',
+    'dependentsCount',
+    'quality',
+    'popularity',
+    'maintenance'
+]
+
+def get_package(package_name: str) -> dict:
     """Query npms.io for package data corresponding to package_name.
     """
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
 
-    data = r'{ "keys": ["react", "firebase"] }'
-    print(data)
-    
-    r = requests.post(f'https://replicate.npmjs.com/_all_docs?include_docs=true',
-                      headers = headers,
-                      data = str(data))
+    if "/" in package_name:
+        package_name = package_name.replace('/', '%2F')
 
-    print(r.text)
+    r = requests.get("https://api.npms.io/v2/package/" + package_name)
+
     # Ensure it doesn't error
     if r.status_code == 200:
         package = json.loads(r.text)
-
-        _trim_package_data(package[package])
-
-        print(package)
-
         return package
     else:
-        raise KeyError('Package Name is invalid, or the webpage is unavailable')
-
+        # raise KeyError('Package Name is invalid, or the webpage is unavailable')
+        print('Error getting package!')
+        return HEADERS
 
 
 def _trim_package_data(data: dict) -> None:
@@ -42,32 +48,46 @@ def _trim_package_data(data: dict) -> None:
     # When we requested the data
     data.pop('analyzedAt')
 
-    # Package release versions
+    data['collected']['metadata'].pop('date')
+    data['collected']['metadata'].pop('author')
+    data['collected']['metadata'].pop('publisher')
+    data['collected']['metadata'].pop('maintainers')
+    data['collected']['metadata'].pop('repository')
+    data['collected']['metadata'].pop('links')
     data['collected']['metadata'].pop('releases')
+    data['collected']['metadata'].pop('hasTestScript')
+    data['collected']['metadata'].pop('hasSelectiveFiles')
 
-    # Package Github Contributors
-    data['collected']['github'].pop('contributors')
-    # Package Github statuses
-    data['collected']['github'].pop('statuses')
+    # NPM internal stuff
+    data['collected'].pop('npm')
+
+    # Package Github Stuff
+    data['collected'].pop('github')
 
     # Remove all source entries
     data['collected'].pop('source')
+    
 
+def _convert_package_json_to_list(data: dict) -> list:
+    """Add the given set of package data to the dataframe."""
+    
+    body = [
+        data.get('collected').get('metadata').get('name'), # str
+        data.get('collected').get('metadata').get('version'), # str
+        data.get('collected').get('metadata').get('description'), # str
+        data.get('collected').get('metadata').get('keywords'), # lst[str]
+        data.get('collected').get('metadata').get('dependencies'), # dict[str, str]
+        data.get('collected').get('metadata').get('devDependencies'), # dict[str, str] 
+        data.get('evaluation').get('popularity').get('communityInterest'), # float
+        data.get('evaluation').get('popularity').get('downloadsCount'), # float
+        data.get('evaluation').get('popularity').get('downloadsAcceleration'), # float
+        data.get('evaluation').get('popularity').get('dependentsCount'), # float
+        data.get('score').get('detail').get('quality'), # float
+        data.get('score').get('detail').get('popularity'), # float
+        data.get('score').get('detail').get('maintenance') # float
+    ]
 
-def get_dependencies():
-    with open('package_list.txt', 'r') as file:
-        data = file.read()
-        i = 0
-        for line in data:
-            r = requests.get("https://api.npms.io/v2/package/" + line)
-            print(i)
-            i += 1
-        
-    # r = requests.get("https://api.npms.io/v2/package/graphql")
-
-    # results = json.loads(r.text)
-    # print(results)
-
+    return body
 
 def write_sample_package_names() -> set[str]:
     """
@@ -88,7 +108,7 @@ def write_sample_package_names() -> set[str]:
     with open('package_list.txt', 'w') as file:
         for package in packages:
             file.write(package + '\n')
-    
+
     return packages
 
 
@@ -98,7 +118,6 @@ def write_popular_package_names() -> set[str]:
     """
     packages = set()
     chars = 'abcdefghijklmnopqrstuvwxyz'
-    chars = 'a'
     for char in chars:
         print(char)
         for i in range(10):
@@ -106,16 +125,68 @@ def write_popular_package_names() -> set[str]:
             r = requests.get("https://api.npms.io/v2/search?from=" + str(start) + "&" + "size=250&q=" + char + "+boost-exact:false")
             results = json.loads(r.text)
             results = results['results']
-            print(results)
             for package in results:
-                packages.add(package['package']['name'])
+                #score = package['score']['detail']['quality'] + package['score']['detail']['maintenance'] + package['score']['detail']['popularity']
+                score = package['searchScore']
+                packages.add((score, package['package']['name']))
+
+    package_list = list(packages)
+    packages_sorted = sorted(package_list)
+
+    package_list = package_list[len(package_list) - 501:]
+
     print('Starting to write')
-    with open('package_list.txt', 'w') as file:
-        for package in packages:
-            file.write(package + '\n')
-    
+    with open('popular.txt', 'w') as file:
+        for package in package_list:
+            file.write(package[1] + '\n')
+
     return packages
 
 
+def get_detailed_data() -> list[list]:
+    """
+    Write to a csv file containing all the information we need about the packages
+    """
+    # Define a dataframe to hold all of the package data
+    #df = pd.DataFrame(columns=HEADERS)
+    packages_so_far = []
+    with open('small_list.txt', 'r') as file:
+        data = file.read()
+        i = 0 # A variable to keep help us keep track of progress
+        for line in data:           
+            packages_so_far.extend(all_package_dependencies(line))
+            print(i)
+            i += 1
+
+    df = pd.DataFrame(packages_so_far, columns=HEADERS)
+    
+    print(df)
+
+    print('Writing DataFrame')
+    df.to_csv('./test.csv')
+        
+
+def all_package_dependencies(package: str) -> list[list]:
+    """
+    Get info for the current package, as well as info for all of the package's upstream dependencies
+    """
+    all_packages = [] # accumulator list
+    # Get data for the current package
+    data = get_package(package)
+    data_list = _convert_package_json_to_list(data)
+    all_packages.append(data_list)
+    # Recursively get data for all of the package's upstream dependencies
+    if data_list[4] is not None:
+        print(package)
+        for package in data_list[4]:            
+            c = all_package_dependencies(package)
+            all_packages.extend(c)
+    
+    return all_packages
+
+
 if __name__ == '__main__':
-    get_packages(['firebase', 'react'])
+    # for i in range(300000):
+    #     print(i)
+    print('Go')
+#     get_packages(['firebase', 'react'])
